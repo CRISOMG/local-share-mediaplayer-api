cd435f20fd856d67392b5a0554b91070a5674f1c
Cristian <cristiancaraballo112@gmail>
2024-11-07 15:54:27 -0400

    details

--- docker-attach.sh
+++ docker-attach.sh
@@ -2,6 +2,7 @@
 
 # docker exec -it express-api bash -c "yarn dev"
 # docker exec -it express-api bash
+# docker attach express-api
 
 if [[ $1  == "dev" ]]; then
     docker exec -it express-api bash -c "yarn dev"
--- scripts/start.sh
+++ scripts/start.sh
@@ -8,6 +8,7 @@
 # pm2 start ecosystem.config.cjs --env production
 # pm2 logs express-api;
 
-# this must be executet by CMD ["pm2-runtime", "start", "/api/ecosystem.config.cjs"] in dockerfile  
+# this must be executet by CMD ["pm2-runtime", "start", "/api/ecosystem.config.cjs"] in dockerfile or ("start": "pm2 start ecosystem.config.cjs") in package.json from yarn start 
+
 pm2 start dist/index.js
 
