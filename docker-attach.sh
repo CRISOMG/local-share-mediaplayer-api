c74bd96dc2b0613edb4d733fc5939c2f25495457
Cristian <cristiancaraballo112@gmail>
2024-11-07 01:51:50 -0400

    fix

--- docker-attach.sh
+++ docker-attach.sh
@@ -3,7 +3,7 @@
 # docker exec -it express-api bash -c "yarn dev"
 # docker exec -it express-api bash
 
-if [[ $1 -eq "dev" ]]; then
+if [[ $1  == "dev" ]]; then
     docker exec -it express-api bash -c "yarn dev"
 else
     docker exec -it express-api bash
